544fe46c7338a6401aea35a9f375662c92851b06
Tom Bandy <hello@tombandy.com>
2021-07-07 18:49:56 +0100

    import order

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
 import { useState, useEffect } from "react";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
 import About from "./components/About";
